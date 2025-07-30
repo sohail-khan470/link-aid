6b979a20118c9480ddd0baa76e03e69063178fd7
shamimakhonzada <shamim.akhonzada@gmail.com>
2025-07-30 10:38:12 +0500

    feat: super admin now add tow company and other changes

--- src/components/ecommerce/ActionsLogChart.tsx
+++ src/components/ecommerce/ActionsLogChart.tsx
@@ -96,7 +96,7 @@ export default function ActionsLogChart() {
   return loading ? (
     <LoadingSpinner />
   ) : (
-    <div className="rounded-2xl border border-gray-200 bg-white px-5 pb-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
+    <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
       {/* Header */}
       <div className="flex flex-col gap-5 mb-6 sm:flex-row sm:justify-between">
         <div className="w-full">
